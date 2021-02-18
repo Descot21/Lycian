58bdd528bac307d21a7ede78a5073890fae7501b
Neel Smith <nsmith@Technites.local>
2021-02-18 02:48:37 -0500

    Build directories

--- scripts/editions.jl
+++ scripts/editions.jl
@@ -3,3 +3,36 @@ using Pkg
 Pkg.activate(".")
 
 using EditorsRepo
+using CitableText
+using CSV
+
+function citation(repo::EditingRepository)
+	arr = CSV.File(repo.root * "/" * repo.configs * "/citation.cex", skipto=2, delim="|", 
+	quotechar='&', esca pechar='&') |> Array
+end
+
+function shorttitle(csvrow)
+    string("*", csvrow.groupName, "*, ", csvrow.workTitle)
+end
+
+function editiondir(csvrow, basedir)
+    urn = CtsUrn(csvrow.urn)
+    parts = workparts(urn)
+    editiondir = basedir * string(parts[1], "_", parts[2])
+    if !isdir(editiondir)
+        mkdir(editiondir)
+    end
+end
+
+root = dirname(pwd())
+textroot = root * "/offline/Texts/" 
+repo = EditingRepository(root, "editions", "dse", "config")
+textcat = textcatalog(repo, "catalog.cex")
+online = filter(row -> row.online, textcat)
+
+for txt in online
+    editiondir(txt, textroot)
+end
+
+
+citeconf = citation(repo)
