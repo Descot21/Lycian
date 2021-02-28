f5a8df8368228742142b883b3905b07e7eab9925
Neel Smith <dnsmith.neel@gmail.com>
2021-02-28 14:08:29 -0500

    Modularize script

--- scripts/lexicon.jl
+++ scripts/lexicon.jl
@@ -7,20 +7,31 @@ using CitableText
 using DataFrames
 
 root = dirname(pwd())
-lexiconfile = root * "/lexicon/hc.cex"
-offlinelex = root * "/offline/Lexicon/"
 
-lexicon = CSV.File(lexiconfile, delim="|") |> Array
-xcribed = filter(lex -> ! ismissing(lex.lemma), lexicon)
+function lexiconFile(basedir)
+    basedir * "/lexicon/hc.cex"
+end
 
-urns = map(l -> l.urn, xcribed)
-lemmas = map(l -> l.lemma, xcribed)
-xlits = map(l -> l.xlit, xcribed)
-articles = map(l -> l.article, xcribed)
-letters = map(l -> replace(l.lemma, "-" => "")[1], xcribed)
+function targetDir(basedir)
+    basedir * "/offline/Lexicon/"
+end
 
-lexdf = DataFrame(letter = letters, urn = urns, lemma = lemmas, xlit = xlits, article = articles)
-grouped = groupby(lexdf, :letter)
+
+"Build a DataFrame for our lexicon data."
+function loadLexiconDF(basedir)
+    lexiconfile = lexiconFile(basedir)
+    lexicon = CSV.File(lexiconfile, delim="|") |> Array
+    xcribed = filter(lex -> ! ismissing(lex.lemma), lexicon)
+
+    urns = map(l -> l.urn, xcribed)
+    lemmas = map(l -> l.lemma, xcribed)
+    xlits = map(l -> l.xlit, xcribed)
+    articles = map(l -> l.article, xcribed)
+    letters = map(l -> replace(l.lemma, "-" => "")[1], xcribed)
+
+    DataFrame(letter = letters, urn = urns, lemma = lemmas, xlit = xlits, article = articles)
+end
+
 
 
 function yamlplus(ltr, xlit)
@@ -50,22 +61,19 @@ function mdrow(entry)
     string("- ", entry.lemma, " (", entry.xlit, ") ", entry.article)
 end
 
-for k in keys(grouped)
-    string(offlinelex, k.letter)
+
+function printLetter(dictentries, outfile, letter)
+    xlitletter =  dictentries[1, :xlit][1]
     
-    entries = grouped[k]
-    xlitletter =  grouped[1][1, :xlit][1]
-    outfile = outputfile(offlinelex, string("Letter ", k.letter))
-    top = yamlplus(k.letter, xlitletter)
+    top = yamlplus(letter, xlitletter)
     mdlines = []
-    for entry in eachrow(entries)
+    for entry in eachrow(dictentries)
         push!(mdlines, mdrow(entry))
         #println(mdrow(entry))
     end
     
     println("Writing to " * outfile)
     println("With ", length(mdlines), " lines of entries." )
-
     doc = top * join(mdlines,"\n")
     open(outfile, "w") do io
         try 
@@ -75,3 +83,22 @@ for k in keys(grouped)
         end
     end 
 end
+
+
+function printLexicon(root)
+    offlinelex = targetDir(root)
+
+    # The lexicon
+    lexdf = loadLexiconDF(root)
+    # Grouped by letter
+    grouped = groupby(lexdf, :letter)
+
+    for k in keys(grouped)
+        string(offlinelex, k.letter)
+        entries = grouped[k]
+        #xlitletter =  grouped[k][1, :xlit][1]
+        outfile = outputfile(offlinelex, string("Letter ", k.letter))
+        printLetter(entries, outfile, k.letter)
+    end
+end
+
