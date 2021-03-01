9144401f42e7cf3714776a1c3f20e0f27582f288
Neel Smith <nsmith@Technites.local>
2021-02-28 20:52:44 -0500

    Add start on name index

---
+++ scripts/nameindex.jl
@@ -0,0 +1,28 @@
+# Run this script from the scripts directory
+using Pkg
+Pkg.activate(".")
+
+root = dirname(pwd())
+repo = EditingRepository(root, "editions", "dse", "config")
+textcat = textcatalog(repo, "catalog.cex")
+online = filter(row -> row.online, textcat)
+
+function citation(repo::EditingRepository)
+	arr = CSV.File(repo.root * "/" * repo.configs * "/citation.cex", skipto=2, delim="|", 
+	quotechar='&', escapechar='&') |> Array
+end
+
+
+#xml = textforurn(repo, urn)
+#converter = o2converter(repo, urn)
+
+for txt in online
+    urn = CtsUrn(txt.urn)
+    println(urn)
+    converter = o2converter(repo, urn)
+    println(converter)
+    xml = textforurn(repo, urn)	
+    #println(xml)
+    #converted = converter(xml, urn)
+    #println(converted)
+end
