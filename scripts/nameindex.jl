9a6b78e737e23a8cbbcfb4e1d8cb0afaaade6cad
Neel Smith <nsmith@Technites.local>
2021-03-01 01:54:51 -0500

    Build citable xml corpus

--- scripts/nameindex.jl
+++ scripts/nameindex.jl
@@ -2,28 +2,36 @@
 using Pkg
 Pkg.activate(".")
 
+using CitableText
+using CSV
+using EditorsRepo
+using EzXML
+
 root = dirname(pwd())
 repo = EditingRepository(root, "editions", "dse", "config")
-textcat = textcatalog(repo, "catalog.cex")
-online = filter(row -> row.online, textcat)
 
+#=
 function citation(repo::EditingRepository)
 	arr = CSV.File(repo.root * "/" * repo.configs * "/citation.cex", skipto=2, delim="|", 
 	quotechar='&', escapechar='&') |> Array
 end
+=#
 
+# This should be in EditorsRepo!
+function xmlcorpus(repo::EditingRepository)
+    textcat = textcatalog(repo, "catalog.cex")
+    online = filter(row -> row.online, textcat)
+    corpora = []
+    for txt in online
+        urn = CtsUrn(txt.urn)
+        reader = o2converter(repo, urn) |> Meta.parse |> eval
+        xml = textforurn(repo, urn)	
+        push!(corpora, reader(xml, urn))
+    end
+    composite_array(corpora)
+end
 
-#xml = textforurn(repo, urn)
-#converter = o2converter(repo, urn)
+c = xmlcorpus(repo)
+for cn in cn.corpus
 
-for txt in online
-    urn = CtsUrn(txt.urn)
-    println(urn)
-    converter = o2converter(repo, urn)
-    println(converter)
-    xml = textforurn(repo, urn)	
-    #ohco2forurn(CONF,urn)
-    #println(xml)
-    #converted = converter(xml, urn)
-    #println(converted)
 end
